18caa3b6de44de965104f75083f892d0fe84f622
Nabil AIT GACEM <aitgacema@gmail.com>
2022-08-27 07:55:18 +0100

    added more instructions

--- stacker.java
+++ stacker.java
@@ -1,5 +1,6 @@
 
 import java.util.Stack;
+import java.util.Arrays;
 import java.util.Scanner;
 //import print in java
 import java.lang.System;
@@ -40,9 +41,8 @@ public class stacker {
     }// end main
 
     private static void parser(Stack<String> st, String instruction) {
-        int op1, op2;
-        op1 = Integer.parseInt(st.pop());
-        op2 = Integer.parseInt(st.pop());
+        Integer op1 = pop_top_element(st);
+        Integer op2 = pop_top_element(st);
 
         switch (instruction) {
             case "add":
@@ -60,10 +60,101 @@ public class stacker {
             case "mod":
                 st.push(Integer.toString(op1 % op2));
                 break;
-        }
+            case "neg":
+                st.push(Integer.toString(op2));
+                st.push(Integer.toString(-op1));
+                break;
+            case "swap":
+                st.push(Integer.toString(op1));
+                st.push(Integer.toString(op2));
+                break;
+            case "dup":
+                st.push(Integer.toString(op1));
+                st.push(Integer.toString(op1));
+                break;
+            case "drop":
+                st.push(Integer.toString(op2));
+                break;
+            case "over":
+                restore_stack(st, op1, op2);
+                st.push(Integer.toString(op2));
+                break;
+            case "rot":
+                int op3 = pop_top_element(st);
+                st.push(Integer.toString(op1));
+                st.push(Integer.toString(op2));
+                st.push(Integer.toString(op3));
+                break;
+            case "print":
+                restore_stack(st, op1, op2);
+                System.out.println(op1);
+                break;
+            case "printall":
+                restore_stack(st, op1, op2);
+                System.out.println(Arrays.toString(st.toArray()));
+                break;
+            case "clear":
+                st.clear();
+                break;
+            case "size":
+                restore_stack(st, op1, op2);
+                System.out.println(st.size());
+                break;
+            case "help":
+                restore_stack(st, op1, op2);
+                display_help();
+                break;
+            case "exit":
+                System.out.println("Program terminated");
+                System.exit(0);
+                break;
 
-        System.out.println("> " + st.peek());
+            default:
+                restore_stack(st, op1, op2);
+                System.out.println("Invalid instruction");
+                break;
+
+        }// end switch
+        if (!st.isEmpty()) {
+            System.out.println("Top of stack > " + st.peek());
+        }
 
     }
 
+    private static Integer pop_top_element(Stack<String> st) {
+        if (!st.isEmpty()) {
+            return Integer.parseInt(st.pop());
+        } else {
+            return null;
+        }
+    }
+
+    private static void restore_stack(Stack<String> st, Integer op1, Integer op2) {
+        if (op2 != null)
+            st.push(Integer.toString(op2));
+        if (op1 != null)
+            st.push(Integer.toString(op1));
+
+    }
+
+    private static void display_help(void){
+        System.out.println("push <number> : push a number to the stack");
+        System.out.println("add : add the top two numbers on the stack");
+        System.out.println("sub : subtract the top two numbers on the stack");
+        System.out.println("mul : multiply the top two numbers on the stack");
+        System.out.println("div : divide the top two numbers on the stack");
+        System.out.println("mod : get the remainder of the top two numbers on the stack");
+        System.out.println("neg : negate the top number on the stack");
+        System.out.println("swap : swap the top two numbers on the stack");
+        System.out.println("dup : duplicate the top number on the stack");
+        System.out.println("drop : drop the top number on the stack");
+        System.out.println("over : copy the second number on the stack to the top");
+        System.out.println("rot : rotate the top three numbers on the stack");
+        System.out.println("print : print the top number on the stack");
+        System.out.println("printall : print all the numbers on the stack");
+        System.out.println("clear : clear the stack");
+        System.out.println("size : print the size of the stack");
+        System.out.println("help : print this help message");
+        System.out.println("exit | halt : exit the program");
+    }
 }
